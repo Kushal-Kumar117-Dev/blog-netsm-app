68ebd45f4ce82e2f0911a95d69d7a9937d3d4d7e
Kushal Kumar <kushal.kumar117@gmail.com>
2025-01-19 13:44:25 +0530

    db-connect-view-added

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,16 @@
 import express from "express";
 import routes from "./routes";
-
+import path from "path";
+import dotenv from "dotenv";
+dotenv.config();
 const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.use('/', routes);
 
 export default app;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,45 @@
 import { Router, Request, Response } from "express";
 const router = Router();
 
+router.get("/", (req: Request, res: Response) => {
+    res.render("index");
+});
+
+router.get("/blogs/create", (req: Request, res: Response) => {
+    res.render("blogs/create");
+});
+
+router.get("/blogs/edit/:id", async (req: Request, res: Response) => {
+    const { id } = req.params;
+    // try {
+    //   const [rows] = await db.query("SELECT * FROM blogs WHERE id = ?", [id]);
+    //   if (rows.length === 0) {
+    //     return res.status(404).send("Blog not found");
+    //   }
+    //   res.render("blogs/edit", { blog: rows[0] }); // Pass blog data to the view
+    // } catch (error) {
+    //   res.status(500).json({ error: error.message });
+    // }
+  });
+
+  // Update a blog
+router.post("/blogs/:id/edit", async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const { title, content } = req.body;
+    // try {
+    //   const [result] = await db.query("UPDATE blogs SET title = ?, content = ? WHERE id = ?", [
+    //     title,
+    //     content,
+    //     id,
+    //   ]);
+    //   if ((result as any).affectedRows === 0) {
+    //     return res.status(404).send("Blog not found");
+    //   }
+    //   res.redirect("/blogs"); // Redirect to blog list after updating
+    // } catch (error) {
+    //   res.status(500).json({ error: error.message });
+    // }
+  });
+  
+
 export default router;
